72b2d2a43b42ba1a26cb793142fb4715a645d5d5
Chris D'Amato <chris@chrisdamato.com>
2011-10-06 05:02:54 -0400

    Cleanup unwanted files

--- untitled.js
+++
@@ -1,3 +0,0 @@
-(function(obj) {
-var __p=[],print=function(){__p.push.apply(__p,arguments);};with(obj||{}){__p.push('\n        <select id="selectProfile">\n            '); _each(models, function(model){  __p.push('\n                <option value="', model.attributes.id ,'">', model.attributes.name ,'</option>\n            ');  }); __p.push('\n        </select>\n    ');}return __p.join('');
-})
